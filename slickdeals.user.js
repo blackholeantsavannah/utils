060a1b5564ca70fa5b6e3e984e47585fb90b8d2f
blackholeantsavannah <ID+blackholeantsavannah@users.noreply.github.com>
2021-12-13 08:36:18 -0500

    slickdeals: 1.1.0: update dealpost to new layout

--- slickdeals.user.js
+++ slickdeals.user.js
@@ -2,18 +2,21 @@
 // @name        slickdeals
 // @namespace   blackholeantsavannah
 // @include     https://slickdeals.net/*
-// @version     1
+// @version     1.1.0
 // @require     https://code.jquery.com/jquery-2.2.4.min.js
 // @grant       none
 // ==/UserScript==
 
 var url_pat_u2 = /https?.*u2=(.*)/
-//var urls_u2 = $('a').filter(function() { return this.href.match(url_pat) } )
 
-$('a').each(function() {
-    if (url_pat_u2.test(this.href)) {
-        console.log('old: ' + this.href);
-        this.href = decodeURIComponent(this.href.replace(url_pat_u2, "$1"));
-        console.log('new: ' + this.href);
-    }
+$(document).ready(function(){
+    $('a').each(function() {
+        if (url_pat_u2.test(this.href)) {
+            console.log('old: ' + this.href);
+            this.href = decodeURIComponent(this.href.replace(url_pat_u2, "$1"));
+            console.log('new: ' + this.href);
+        }
+    })
+
+    $('.originalPoster_post').each(function(){$(this).addClass('originalPoster_post--visible')});
 })
